03c9afff4c36ed2964efdedbe1ddb4cf2d15f69a
avbhutani <avbhutani3@gmail.com>
2024-10-19 20:22:19 +0530

    Add/ Section#2 Feature Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import video1 from './assets/video1.mp4'
 import video2 from './assets/video2.mp4'
+import FeatureSection from "./components/FeatureSection";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
 
       <div className="max-w-7xl mx-auto pt-20 px-6">
       <HeroSection />
+      <FeatureSection />
       </div>
     </>
 
