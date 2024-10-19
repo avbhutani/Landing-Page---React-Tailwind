6b9cb6fdf251632d38fae00f02e40b0bcb2f3857
avbhutani <avbhutani3@gmail.com>
2024-10-19 20:51:03 +0530

    Add/ Accelerate Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import video1 from './assets/video1.mp4'
 import video2 from './assets/video2.mp4'
 import FeatureSection from "./components/FeatureSection";
+import WorkflowSection from "./components/WorkflowSection";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <div className="max-w-7xl mx-auto pt-20 px-6">
       <HeroSection />
       <FeatureSection />
+      <WorkflowSection />
       </div>
     </>
 
