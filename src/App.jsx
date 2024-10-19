0d69cad9150555e53339bb2ca2e4568f3d285f59
avbhutani <avbhutani3@gmail.com>
2024-10-19 19:52:25 +0530

    Initial Commit/ Added Header and First section

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+import video1 from './assets/video1.mp4'
+import video2 from './assets/video2.mp4'
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+      <HeroSection />
+      </div>
+    </>
+
+  )
+}
